9096a4b2048857ddc7fb968919b8e8cfc528c331
jakecronin <jake.cronin@emory.edu>
2017-04-21 16:51:08 -0400

    Fixed 2d and 1d hinges. Added rotating cannons

---
+++ Assets/scripts/aimCannon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aimCannon : MonoBehaviour {
+
+	public Transform target;
+	public Transform foundation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (target, foundation.up); 
+		//make cannont .front face person
+	}
+}
